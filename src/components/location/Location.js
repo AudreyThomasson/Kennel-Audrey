16d0238dae3a5afb37199fe1e16b4985c2fdfe76
Audrey Thomasson <audrey@macbook-pro.lan>
2020-10-02 11:57:57 -0500

    started code

---
+++ src/components/location/Location.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export const LocationCard = () => (
+    <section className="location">
+        <h3 className="location__name">Nashville Kennels Noth</h3>
+        <div className="location__address">200 Main Street</div>
+    </section>
+)
